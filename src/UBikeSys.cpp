d63505ef4471be7232770e0cd52a2f7b7780e998
qmo1222 <howard19192828@gmail.com>
2017-06-07 00:28:10 +0800

    add ubike system class

---
+++ src/UBikeSys.cpp
@@ -0,0 +1,46 @@
+#include "UBikeSys.h"
+
+// build the station graph
+void UBikeSys::buildStation(string s, string t, int dist){
+	cout<<"buildStation function still making"<<endl;
+}
+// add new bike to company
+void UBikeSys::newBike(string bikeClass, string license, int mile, string station){
+	cout<<"newBike function still making"<<endl;
+}
+// remove bike from company
+void UBikeSys::junkIt(string license){
+	cout<<"junkIt function still making"<<endl;
+}
+// rent the bike
+void UBikeSys::rent(string station, string bikeClass){
+	cout<<"rent function still making"<<endl;
+}
+// return bike
+void UBikeSys::returns(string station, string license, int mile){
+	cout<<"returns function still making"<<endl;
+}
+// transfer bike to other station
+void UBikeSys::trans(string station, string license){
+	cout<<"trans function still making"<<endl;
+}
+// inquire certain bike
+void UBikeSys::inquire(string license){
+	cout<<"inquire function still making"<<endl;
+}
+// report certain station
+void UBikeSys::stationReport(string station){
+	cout<<"stationReport function still making"<<endl;
+}
+// report all bikes info
+void UBikeSys::uBikeReport(){
+	cout<<"uBikeReport function still making"<<endl;
+}
+// report money earn by the station
+void UBikeSys::netSearch(string station){
+	cout<<"netSearch function still making"<<endl;
+}
+// print binary search tree
+void UBikeSys::bReport(){
+	cout<<"bReport function still making"<<endl;
+}
---
+++ src/UBikeSys.h
@@ -0,0 +1,39 @@
+#ifndef UBikeSys_h
+#define UBikeSys_h
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class UBikeSys
+{
+public:
+	// UBikeSys(); // constructor
+	// StationGraph mrt; // graph
+
+	// build the station graph
+	void buildStation(string s, string t, int dist);
+	// add new bike to company
+    void newBike(string bikeClass, string license, int mile, string station);
+    // remove bike from company
+    void junkIt(string license);
+    // rent the bike
+    void rent(string station, string bikeClass);
+    // return bike
+    void returns(string station, string license, int mile);
+    // transfer bike to other station
+    void trans(string station, string license);
+	// inquire certain bike
+	void inquire(string license);
+	// report certain station
+	void stationReport(string station);
+	// report all bikes info
+	void uBikeReport();
+	// report money earn by the station
+	void netSearch(string station);
+	// print binary search tree
+	void bReport();
+};
+
+#endif
